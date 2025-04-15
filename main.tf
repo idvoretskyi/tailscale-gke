b8ec5956c94a11a4c4556030b7d19332d64800e4
Ihor Dvoretskyi <ihor@linux.com>
2025-04-15 16:31:25 +0300

    Refactor Kubernetes and Helm providers to conditionally connect based on cluster existence
    Signed-off-by: Ihor Dvoretskyi <ihor@linux.com>

--- main.tf
+++ main.tf
@@ -21,31 +21,41 @@ provider "google" {
   region  = var.region
 }
 
+data "google_client_config" "default" {}
+
+# Use a local variable to determine if we should try to connect to the cluster
+locals {
+  cluster_exists = try(module.gke.endpoint != "", false)
+}
+
+# Kubernetes provider with ignore_errors to help with destroy operations
 provider "kubernetes" {
-  host                   = "https://${module.gke.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+  host                   = local.cluster_exists ? "https://${module.gke.endpoint}" : "https://localhost"
+  token                  = local.cluster_exists ? data.google_client_config.default.access_token : ""
+  cluster_ca_certificate = local.cluster_exists ? base64decode(module.gke.ca_certificate) : ""
+
+  # This will ignore API errors when destroying resources that might not exist anymore
+  ignore_annotations = [".*"]
+  ignore_labels = [".*"]
 }
 
 provider "helm" {
   kubernetes {
-    host                   = "https://${module.gke.endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+    host                   = local.cluster_exists ? "https://${module.gke.endpoint}" : "https://localhost"
+    token                  = local.cluster_exists ? data.google_client_config.default.access_token : ""
+    cluster_ca_certificate = local.cluster_exists ? base64decode(module.gke.ca_certificate) : ""
   }
 }
-
-data "google_client_config" "default" {}
 
 module "gke" {
   source                = "./modules/gke"
   project_id            = var.project_id
   region                = var.region
-  zone                  = var.zone # Pass zone for zonal clusters (cost-effective)
+  zone                  = var.zone
   cluster_name          = var.cluster_name
   network               = var.network
   subnetwork            = var.subnetwork
-  node_count            = var.node_count # Will be used if autoscaling is disabled
+  node_count            = var.node_count
   min_node_count        = var.min_node_count
   max_node_count        = var.max_node_count
   machine_type          = var.machine_type
@@ -54,6 +64,7 @@ module "gke" {
   master_ipv4_cidr_block = var.master_ipv4_cidr_block
 }
 
+# Make Tailscale module resources optional for easier destruction
 module "tailscale" {
   source     = "./modules/tailscale"
   auth_key   = var.tailscale_auth_key
